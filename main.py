306c9f5dc1409db349e8d80e439653a93a8e87ef
Eagle-008 <38664630+Eagle-008@users.noreply.github.com>
2025-10-16 17:59:35 +0300

    feat: added username logging

--- main.py
+++ main.py
@@ -64,27 +64,27 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # await update.message.reply_text(update.message.text)
 
 async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = update.effective_user.id
+    user = update.effective_user
     # Save the user_id to users.txt file
     with open("users.txt", "a") as f:
-        f.write(f"{user_id}\n")
-    logger.warning(f"User {user_id} subscribed.")
+        f.write(f"{user.id}\n")
+    logger.warning(f"User {user.username} ({user.id}) subscribed.")
     await update.message.reply_text("Вы подписались на обновления о новых вакансиях!")
 
 async def unsubscribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = update.effective_user.id
+    user = update.effective_user
     try:
         with open("users.txt", "r") as f:
             users = f.readlines()
         users = [u.strip() for u in users if u.strip()]
-        if str(user_id) not in users:
+        if str(user.id) not in users:
             await update.message.reply_text("Вы не были подписаны.")
         else:
-            users = [u for u in users if u != str(user_id)]
+            users = [u for u in users if u != str(user.id)]
             with open("users.txt", "w") as f:
                 for u in users:
                     f.write(f"{u}\n")
-        logger.warning(f"User {user_id} unsubscribed.")
+        logger.warning(f"User {user.username} ({user.id}) unsubscribed.")
         await update.message.reply_text("Вы отписались от обновлений о вакансиях.")
     except FileNotFoundError:
         await update.message.reply_text("Внутренняя ошибка: база пользователей не найдена.")
